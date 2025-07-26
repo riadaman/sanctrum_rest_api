7f5e793194a469391dbb285842410768c0e62969
riadaman <riadmohammod2000@gmail.com>
2025-07-27 00:55:21 +0600

    create token for user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Helper\ResponseHelper;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use Exception;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
@@ -48,18 +50,49 @@ class AuthController extends Controller
              return   ResponseHelper::error(
                 'User registration failed',
                 'error',
-                400
+                500
                 );
         }
     }
 
 
     /**
-     * Update the specified resource in storage.
+     * Function to handle user login.
+     * @param LoginRequest $request
+     * @return JsonResponse
      */
-    public function update(Request $request, string $id)
+    public function login(LoginRequest $request)
     {
-        //
+       try{
+            if(!Auth::attempt(['email' => $request->email,'password' => $request->password,])){
+                return ResponseHelper::error(
+                    'Invalid credentials',
+                    'error',
+                    401
+                );
+            }
+            $user = Auth::user();
+            // dd($user);
+            // create a new token for the user
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return ResponseHelper::success(
+                [
+                    'user' => $user,
+                    'token' => $token
+                ],
+                'User logged in successfully',
+                'success',
+                200
+            );
+       }
+         catch (Exception $e) {
+                Log::error('User login failed: ' . $e->getMessage() . '-Line: ' . $e->getLine());
+                return ResponseHelper::error(
+                 'User login failed',
+                 'error',
+                 500
+                );
+          }
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
+    Route::post('login', 'login');
 });
 
