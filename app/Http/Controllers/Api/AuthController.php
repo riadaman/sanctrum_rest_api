1af317cb04a1fcccbe654abcd6e7f267b3c74410
riadaman <riadmohammod2000@gmail.com>
2025-07-27 00:00:06 +0600

    add: helper function for returing common respone

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Helper\ResponseHelper;
 use App\Models\User;
 use Exception;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
@@ -28,20 +31,28 @@ class AuthController extends Controller
                 'phone_number' => $request->phone_number,
             ]);
             if($user){
-                
+             return   ResponseHelper::success(
+                    $user,
+                    'User registered successfully',
+                    'success',
+                    201
+                );
             }
+            return   ResponseHelper::error(
+                'User registration failed',
+                'error',
+                400
+                );
         } catch (user $e) {
-            
+            Log::error('User registration failed: ' . $e->getMessage()) . '-Line: ' . $e->getLine();
+             return   ResponseHelper::error(
+                'User registration failed',
+                'error',
+                400
+                );
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -51,11 +62,4 @@ class AuthController extends Controller
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
